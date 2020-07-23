55a14ec3c24849c6fcd9d681a0d9575ad205b548
Nguyen Viet My <zzvietmyzz@gmail.com>
2020-07-23 10:38:16 +0700

    Project set up

---
+++ TheMovieWithAlamofire/List/ListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ListViewController.swift
+//  TheMovieWithAlamofire
+//
+//  Created by MM on 7/23/20.
+//  Copyright © 2020 MM. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationItem.title = "List"
+    }
+}
